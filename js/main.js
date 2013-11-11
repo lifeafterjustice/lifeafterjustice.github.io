8f6c91610f10228735bdcd0d9e719a4d11b2e75c
Scott Weisman <scott@launchpadlab.com>
2013-11-10 23:49:42 -0600

    highlight current li

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+	var pathname = window.location.pathname;
 
+	$(".nav li").each(function(index) {
+	    if (pathname.toUpperCase().indexOf($(this).text().toUpperCase()) != -1)
+	        $(this).addClass("active");
+	});
+
+	if ($(".nav li.active").length == 0)
+	    $("li#home").addClass("active");
+});
